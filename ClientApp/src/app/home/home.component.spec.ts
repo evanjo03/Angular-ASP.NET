b5bbe8be4743f132ae32df8956c392f845ed8efc
John <john.evans@cpsi.com>
2019-11-27 12:02:45 -0600

    More Karma

--- ClientApp/src/app/home/home.component.spec.ts
+++ ClientApp/src/app/home/home.component.spec.ts
@@ -1,10 +1,39 @@
 import { HomeComponent } from './home.component';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {CommonModule} from '@angular/common';
 
 fdescribe('figuring out how testing works', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
 
-  it('does it do the thing?', () => {
-    const a = 3;
-    expect(a).toBeDefined();
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      imports: [CommonModule],
+      providers: [],
+      declarations: [HomeComponent]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  })
+
+  describe('add', function() {
+    it('should log the result', () => {
+      spyOn(console, 'log').and.callThrough();
+      spyOn(component, 'add').and.callThrough();
+
+      let final = component.add(4, 5);
+      expect(component.add).toHaveBeenCalled();
+      expect(console.log).toHaveBeenCalled();
+      expect(final).toBe(9);
+    });
+
   });
 });
 
--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+
+  add(a1: number, b1: number) {
+    console.log(a1 + b1);
+    return a1 + b1;
+  }
+
 }
