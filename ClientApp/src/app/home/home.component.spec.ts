27bfad3162d2357d9ddbce87de9e3fc518989931
John <john.evans@cpsi.com>
2019-11-27 08:29:04 -0600

    Learning Karma

---
+++ ClientApp/src/app/home/home.component.spec.ts
@@ -0,0 +1,11 @@
+import { HomeComponent } from './home.component';
+
+fdescribe('figuring out how testing works', () => {
+
+  it('does it do the thing?', () => {
+    const a = 3;
+    expect(a).toBeDefined();
+  });
+});
+
+
