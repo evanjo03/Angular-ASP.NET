964a2d2ee89ad45701cc35de06c5a47fe3d93055
John <john.evans@cpsi.com>
2019-11-27 13:50:58 -0600

    Messing around with api

--- ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -7,11 +7,18 @@ import { HttpClient } from '@angular/common/http';
 })
 export class FetchDataComponent {
   public forecasts: WeatherForecast[];
+  public things: AnotherList[];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     http.get<WeatherForecast[]>(baseUrl + 'api/SampleData/WeatherForecasts').subscribe(result => {
       this.forecasts = result;
+      console.log(baseUrl);
     }, error => console.error(error));
+
+    // http.get<AnotherList[]>(baseUrl + 'api/SampleData/AnotherList').subscribe(result => {
+    //   this.forecasts = result;
+    //   console.log(baseUrl);
+    // }, error => console.error(error));
   }
 }
 
@@ -21,3 +28,10 @@ interface WeatherForecast {
   temperatureF: number;
   summary: string;
 }
+
+// interface AnotherList {
+//   dateFormatted: string;
+//   temperatureC: number;
+//   temperatureF: number;
+//   summary: string;
+// }
